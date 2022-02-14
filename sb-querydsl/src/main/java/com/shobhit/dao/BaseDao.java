870a496618c7a5c2503ba86184c9e2188c690d6e
Shobhit Bhardwaj <successshobhit@gmail.com>
2022-02-14 18:55:35 +0530

    Changing Repository of DB Projects

--- sb-querydsl/src/main/java/com/shobhit/dao/BaseDao.java
+++
@@ -1,9 +0,0 @@
-package com.shobhit.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface BaseDao<T, ID> extends JpaRepository<T, ID> {
-	public T findByIdMandatory(ID id) throws IllegalArgumentException;
-}
