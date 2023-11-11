0c070c9bba97679627ad56b767f9de45080bcf70
Shobhit Bhardwaj <successshobhit@gmail.com>
2023-11-11 22:55:03 +0530

    Spring Boot Kubernetes Example

---
+++ sb-k8s-basic/src/main/java/com/shobhit/k8s/SBK8SBasicApplication.java
@@ -0,0 +1,22 @@
+package com.shobhit.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SBK8SBasicApplication {
+
+	@GetMapping("/")
+	public String hello() {
+		System.out.println("Request Received for GET hello API.");
+
+		return "Hello From Spring Boot Kubernetes";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBK8SBasicApplication.class, args);
+	}
+}
